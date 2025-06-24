3b06906473649570aaf3c08f0e44be535825ff2d
STD <sergitorresdalmau@gmail.com>
2025-06-24 14:13:22 +0200

    Started Case Opener

---
+++ case.js
@@ -0,0 +1,103 @@
+const skins = [
+    // --- Special Item (Dorado) ---
+    {
+        nombre: 'Bayonet | Fade',
+        fuente: 'resources/images/bayonetFade.webp',
+        rareza: 'Special Item',
+        prob: 1 // Muy raro
+    },
+    // --- Covert (Rojo) ---
+    {
+        nombre: 'AK-47 | Fire Serpent',
+        fuente: 'resources/images/ak.webp',
+        rareza: 'Covert',
+        prob: 5 // Raro
+    },
+    // --- Classified (Rosa) ---
+    {
+        nombre: 'AWP | Graphite',
+        fuente: 'resources/images/awpGraphite.webp',
+        rareza: 'Classified',
+        prob: 20
+    },
+    {
+        nombre: 'Desert Eagle | Golden Koi',
+        fuente: 'resources/images/deagleGoldenKoi.webp',
+        rareza: 'Classified',
+        prob: 20
+    },
+    {
+        nombre: 'P90 | Emerald Dragon',
+        fuente: 'resources/images/p90EmeraldDragon.webp',
+        rareza: 'Classified',
+        prob: 20
+    },
+    {
+        nombre: 'P2000 | Ocean Foam',
+        fuente: 'resources/images/p2000OceanFoam.webp',
+        rareza: 'Classified',
+        prob: 20
+    },
+    // --- Restricted (Morado) ---
+    {
+        nombre: 'M4A1-S | Bright Water',
+        fuente: 'resources/images/m4a1sbrightWater.webp',
+        rareza: 'Restricted',
+        prob: 50
+    },
+    {
+        nombre: 'M4A4 | Zirka',
+        fuente: 'resources/images/m4a4Zirka.webp',
+        rareza: 'Restricted',
+        prob: 50
+    },
+    {
+        nombre: 'USP-S | Overgrowth',
+        fuente: 'resources/images/uspOvergrowth.webp',
+        rareza: 'Restricted',
+        prob: 50
+    },
+    // --- Mil-Spec (Azul) ---
+    {
+        nombre: 'Dual Berettas | Black Limba',
+        fuente: 'resources/images/dberettasBlackimba.webp',
+        rareza: 'Mil-Spec',
+        prob: 100 // Común
+    },
+    {
+        nombre: 'Galil AR | Shattered',
+        fuente: 'resources/images/GaliArShattered.webp',
+        rareza: 'Mil-Spec',
+        prob: 100
+    },
+    {
+        nombre: 'MAC-10 | Graven',
+        fuente: 'resources/images/mac10Graven.webp',
+        rareza: 'Mil-Spec',
+        prob: 100
+    },
+    {
+        nombre: 'Nova | Tempest',
+        fuente: 'resources/images/NovaTempest.webp',
+        rareza: 'Mil-Spec',
+        prob: 100
+    },
+    {
+        nombre: 'SG 553 | Wave Spray',
+        fuente: 'resources/images/sg553waveSpray.webp',
+        rareza: 'Mil-Spec',
+        prob: 100
+    },
+    {
+        nombre: 'GS3S1 | Demeter', 
+        fuente: 'resources/images/sgDemeter.webp',
+        rareza: 'Mil-Spec',
+        prob: 100
+    },
+    {
+        nombre: 'UMP-45 | Bone Pile',
+        fuente: 'resources/images/ump-45bonePile.webp',
+        rareza: 'Mil-Spec',
+        prob: 100
+    }
+];
